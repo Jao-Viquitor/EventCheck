b9f42085d7abba5468681e9e4d313bb583da709b
Jao-Viquitor <victorsantos.ft18@gmail.com>
2024-08-07 00:08:27 -0300

    Finally :tada:

--- ui/CheckIn.jsx
+++ ui/CheckIn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const CheckInButton = ({ person, handleCheckIn }) => (
     <button
         onClick={() => handleCheckIn(person._id)}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+        className="bg-blue-500 w-64 text-primary px-4 py-2 rounded"
     >
         Check-in {person.firstName} {person.lastName}
     </button>
@@ -12,7 +12,7 @@ export const CheckInButton = ({ person, handleCheckIn }) => (
 export const CheckOutButton = ({ person, handleCheckOut }) => (
     <button
         onClick={() => handleCheckOut(person._id)}
-        className="bg-red-500 text-white px-4 py-2 rounded"
+        className="bg-red-500 w-64 text-primary px-4 py-2 rounded"
     >
         Check-out {person.firstName} {person.lastName}
     </button>
