4dcc2ce10b47ee9d67d1c80aadaa0374c188dbf5
Jao-Viquitor <victorsantos.ft18@gmail.com>
2024-08-07 00:31:30 -0300

    Finally :tada:

--- ui/LogoImage.jsx
+++ ui/LogoImage.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function LogoImage() {
     return (
-        <div className="w-24 h-24 rounded-full mx-auto">
+        <div className="w-24 h-24 mx-auto">
             <img
-                src="../public/logo.png"
+                src="/logo.png"
                 alt="Logo"
-                className="w-24 h-24 rounded-full"
+                className="w-24 h-24"
             />
         </div>
     );
