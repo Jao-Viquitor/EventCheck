bb635a0a5c7cd2a7c36bafbb4aa49c13e2fa961f
Jao-Viquitor <victorsantos.ft18@gmail.com>
2024-08-23 11:54:34 -0300

    [FIX :art: Styles] make Label component responsive with adaptive text size

--- ui/Label.jsx
+++ ui/Label.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Label = ({ htmlFor, children }) => (
     <label
         htmlFor={htmlFor}
-        className="block mb-2 text-sm font-medium text-secondary dark:text-primary"
+        className="block mb-2 text-sm sm:text-base lg:text-lg font-medium text-secondary dark:text-primary"
     >
         {children}
     </label>
