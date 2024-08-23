438c7639bd622fbf31affe2b2147dabf4ef7f662
Jao-Viquitor <victorsantos.ft18@gmail.com>
2024-08-23 11:24:06 -0300

    [FIX :art: Styles] add margin-top to SearchInput on small screens for better spacing

--- ui/SearchInput.jsx
+++ ui/SearchInput.jsx
@@ -6,6 +6,6 @@ export const SearchInput = ({ searchQuery, handleSearchChange }) => (
         value={searchQuery}
         onChange={handleSearchChange}
         placeholder="Search..."
-        className="bg-gray-50 w-7/12 me-6 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 flex-row p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="w-full sm:w-7/12 sm:me-6 sm:mt-4 mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     />
 );
--- ui/SelectFilter.jsx
+++ ui/SelectFilter.jsx
@@ -4,7 +4,7 @@ export const SelectFilter = ({ value, onChange, options }) => (
     <select
         value={value}
         onChange={onChange}
-        className="w-full max-w-xs bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:max-w-sm md:max-w-md lg:max-w-lg"
+        className="w-full sm:w-auto sm:mt-4 mt-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     >
         {options.map((option) => (
             <option key={option.value} value={option.value}>
