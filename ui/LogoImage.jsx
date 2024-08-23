4693433a688f8daeb6c6f50dcc272eb8e25bfefe
Jao-Viquitor <victorsantos.ft18@gmail.com>
2024-08-23 11:52:25 -0300

    [FIX :art: Styles] make LogoImage component responsive for different screen sizes

--- ui/LogoImage.jsx
+++ ui/LogoImage.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export function LogoImage() {
     return (
-        <div className="w-24 h-24 mx-auto">
+        <div className="w-24 h-24 sm:w-32 sm:h-32 lg:w-32 lg:h-32 mx-auto">
             <img
                 src="/logo.png"
                 alt="Logo"
-                className="w-24 h-24"
+                className="w-full h-full object-contain"
             />
         </div>
     );
